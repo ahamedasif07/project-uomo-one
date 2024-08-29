3f6f60a28c0c55a6921ed4e07c1966155e779b68
rxasif07 <rxasif31@gmail.com>
2024-08-29 21:28:34 +0600

    trandy poduct done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Catagory from "../components/Home/Catagory";
 import Header from "../components/Home/Header";
+import TrandyProducts from "../components/Home/TrandyProducts";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
       <Catagory></Catagory>
+      <TrandyProducts></TrandyProducts>
     </div>
   );
 };
---
+++ src/components/Home/TrandyProducts.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const TrandyProducts = () => {
+  return (
+    <div className="container mx-auto pt-[40px] ">
+      <h2 className="text-center text-3xl font-semibold">
+        Our Trendy <span className="font-extrabold">Products</span>
+      </h2>
+      <div>
+        <ul className="flex flex-wrap justify-center gap-5 py-7">
+          <NavLink
+            className="font-semibold text-[14px] text-gray-700 relative pb-1 group"
+            to="/"
+          >
+            ALL
+            <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
+          </NavLink>
+          <NavLink
+            className="font-semibold text-[14px] text-gray-700 relative pb-1 group"
+            to="#"
+          >
+            NEW ARRIVALS
+            <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
+          </NavLink>
+          <NavLink
+            className="font-semibold text-[14px] text-gray-700 relative pb-1 group"
+            to="#"
+          >
+            BEST SELLER
+            <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
+          </NavLink>
+          <NavLink
+            className="font-semibold text-[14px] text-gray-700 relative pb-1 group"
+            to="#"
+          >
+            TOP RATED
+            <span className="absolute left-0 bottom-0 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></span>
+          </NavLink>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TrandyProducts;
