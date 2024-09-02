fac68a9fe8843801ba8a090e0441df8218a20387
rxasif07 <rxasif31@gmail.com>
2024-09-02 10:15:46 +0600

    custom responcive done

--- src/components/Home/Catagory.jsx
+++ src/components/Home/Catagory.jsx
@@ -7,7 +7,7 @@ const Catagory = () => (
   <div>
     <div className="container px-3  mx-auto py-10">
       <div className="flex lg:flex-row flex-col gap-8">
-        <div className="md:w-1/2">
+        <div className="lg:w-1/2">
           <div className="md:px-[60px] px-2 lg:px-0">
             <div
               className="relative mx-auto  md:mx-0 md:h-[600px] h-[300px] md:mb-2 flex text-black flex-col bg-cover bg-center"
@@ -25,7 +25,7 @@ const Catagory = () => (
             </div>
           </div>
         </div>
-        <div className="md:w-1/2">
+        <div className="lg:w-1/2">
           {/* Upper content */}
           <div className="md:px-[60px] px-2 relative flex flex-col md:gap-3 gap-0   lg:px-0">
             <div
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xm: "320px", // Custom size for extra small
+        sm: "520px", // Default small size
+        md: "768px", // Default medium size
+        lg: "1024px", // Default large size
+        xl: "1280px", // Default extra large size
+        "2xl": "1536px", // Default 2xl size
+      },
+    },
   },
   plugins: [],
 };
